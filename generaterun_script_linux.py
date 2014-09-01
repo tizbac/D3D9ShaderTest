a8677861af2ab75eae3fcf51544d6596f15c4271
Tiziano Bacocco <tizbac2@gmail.com>
2014-09-01 19:54:31 +0200

    Removed pregenrated scripts Added mul tests Coloured output

--- generaterun_script_linux.py
+++ generaterun_script_linux.py
@@ -10,6 +10,6 @@ for x in tests:
 for x in tests:
   outf.write("echo \"Checking %s with Win32...\"\n"%x)
   outf.write("./threshold.py compare -metric mae tests/%s/OutputLinux.bmp tests/%s/OutputWin32.bmp null:\n"%(x,x));
-  outf.write("if [ \"$?\" == \"0\" ]; then echo \"-- OK\"; else echo \"-- Failed\"; fi\n");
+  outf.write("if [ \"$?\" == \"0\" ]; then echo -e \"-- \\033[32mOK\\033[0m\"; else echo -e \"\\033[31m-- Failed\\033[0m\"; fi\n");
 outf.close()
 os.chmod("run.sh",448)
